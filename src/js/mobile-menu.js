8d11a2091257c5603a4168986481cf5616c27e00
krisgsnv <krigasf@gmail.com>
2023-07-18 01:37:25 +0300

    add footer, support section

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@ const mobileMenuHandler = (e) => {
   const el = e.target;
   if (el.classList.contains("header__nav-btn")) {
     el.classList.toggle("header__nav-btn_active");
+    document.body.classList.toggle("no-scroll");
     document.querySelector(".header__list")?.classList.toggle("header__list_active");
   }
 };
