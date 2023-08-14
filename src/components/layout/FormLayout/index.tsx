38fd7f0a9e6d7b52e32bd8fb470b7a69be979b5f
krisgsnv <krigasf@gmail.com>
2023-08-14 13:16:30 +0300

    add form-layout

---
+++ src/components/layout/FormLayout/index.tsx
@@ -0,0 +1,13 @@
+type FormLayoutPropsType = {
+  children?: React.ReactNode
+};
+
+const FormLayout = (props:FormLayoutPropsType) => {
+  return (
+    <section className="form-layout">
+        {props.children}
+    </section>
+  );
+};
+
+export default FormLayout;
